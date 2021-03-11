0a880178512952bb05058675d640dd7358b5d4d7
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-11 15:51:47 +0100

    Dodavanje ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,8 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    myQuotient = numOne/numTwo;
+    //Only change code above this line
+    return myQuotient;
+    }
+    console.log(main(numOne, numTwo));
+    module.exports = main;
