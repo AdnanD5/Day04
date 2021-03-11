b80f86c61dbe1e1836d3f8109d98c45089684254
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-11 16:01:42 +0100

    Dodavanje

---
+++ ex07/src/index.js
@@ -0,0 +1,8 @@
+function main(numOne, numTwo){
+  //Only change code  below this line 
+  remainter = numOne % numTwo;
+  //Only change code  above this line
+  return remainter
+}
+console.log(main(numOne, numTwo));
+module.exports = main;
